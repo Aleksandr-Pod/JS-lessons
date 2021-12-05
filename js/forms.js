abd8038e38d3cd43ac2d54873539bdc5f9f16356
Aleksandr-Pod <stdfire@gmail.com>
2021-12-05 22:08:35 +0200

    5-Dec-21

--- js/forms.js
+++ js/forms.js
@@ -1,20 +1,32 @@
-const form = document.querySelector(".js-register-form");
+//        Собрать данные с полей ввода
 
-form.addEventListener('submit', onFormSubmit);
+// const form = document.querySelector(".js-register-form");
 
-function onFormSubmit(ev) {
-    ev.preventDefault();        // Отключаем действие браузера по умолчанию
-    // console.log(ev.currentTarget); // Это ссылка на объект, где произошло событие
-    // console.log(ev.currentTarget.elements); // Элементы объекта, ... 
-    // console.log(ev.currentTarget.elements.subscription.value) // Покажет
-    // результат формы ввода
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(ev) {
+//     ev.preventDefault();        // Отключаем действие браузера по умолчанию
+//     // console.log(ev.currentTarget); // Это ссылка на объект, где произошло событие
+//     // console.log(ev.currentTarget.elements); // Элементы объекта, ... 
+//     // console.log(ev.currentTarget.elements.subscription.value) // Покажет
+//     // результат формы ввода
+
+//             // Класс - FormData
+
+//     //         Собрать значения всех полей формы:
+//     const formData = new FormData(ev.currentTarget);
+//         formData.forEach((value, fieldName) => {
+//         console.log(fieldName, ":", value);
+//     })
+// }
 
 
-    // Класс - FormData
-
-    // Собрать значения всех полей формы:
-    const formData = new FormData(ev.currentTarget);
-        formData.forEach((value, fieldName) => {
-        console.log(fieldName, ":", value);
-    })
+//           Активируем кнопку submit через checkbox
+const refs = {
+    checkLicense: document.querySelector(".js-checkLicense"),
+    formSubmit: document.querySelector(".js-formSubmit"),
+}
+refs.checkLicense.addEventListener('change', onCheckLicense);
+function onCheckLicense(event) {
+    refs.formSubmit.disabled = !event.currentTarget.checked;
 }
